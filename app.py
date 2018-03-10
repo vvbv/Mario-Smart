b36e8d3613085f0cc12dee8519a02597cd8407bf
cjcruzrivera <cruz.camilo@correounivalle.edu.co>
2018-03-09 20:45:45 -0500

    Uso de plantilla

--- app.py
+++ app.py
@@ -7,5 +7,26 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/amplitud')
+def amplitud():
+    return render_template("amplitud.html")
+
+@app.route('/costo')
+def costo():
+    return render_template("costo.html")
+
+@app.route('/profundidad')
+def profundidad():
+    return render_template("profundidad.html")
+
+@app.route('/avara')
+def avara():
+    return render_template("avara.html")
+
+@app.route('/a_estrella')
+def a_estrella():
+    return render_template("a_estrella.html")
+
+
 if __name__ == '__main__':
     app.run(debug = True, host='0.0.0.0')
---
+++ static/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#start").click(function(){
+        // alert("Hola mundo");
+        $("#start").css("display","none");
+        $(".imgTitulo").addClass('small');
+        $(".gif").addClass('small');
+
+    })
+})
